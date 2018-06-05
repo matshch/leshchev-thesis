1e6a79476551f8e4a33a1efae02f72305e5137d1
Artem Leshchev <matshch@gmail.com>
2018-06-06 02:29:50 +0300

    Module of distributed statistics!

--- app.js
+++ app.js
@@ -61,6 +61,78 @@ app.get('/create', (req, res) => {
   })
 })
 
+app.get('/stats', async function (req, res) {
+  const list = await db.list()
+  const vip = list.filter(e => e.vip).length
+  const not_vip = list.length - vip
+
+  const entered = list.filter(e => e.entered).length
+  const not_entered = list.length - entered
+
+  const times = {}
+
+  for (const user of list) {
+    for (const key of Object.keys(user.$times)) {
+      const time = new Date(parseInt(user.$times[key]))
+      time.setSeconds(0)
+      time.setMilliseconds(0)
+      const label = time.toISOString()
+      if (!times[label]) {
+        times[label] = 1
+      } else {
+        times[label] += 1
+      }
+    }
+  }
+  const graph = Object.keys(times).map(e => ({
+    x: e,
+    y: times[e]
+  }))
+  const fullGraph = [...graph]
+  for (const point of graph) {
+    let time = new Date(point.x)
+    time.setMinutes(time.getMinutes() + 1)
+    let label = time.toISOString()
+    if (!times[label]) {
+      fullGraph.push({
+        x: label,
+        y: 0
+      })
+      times[label] = 0
+    }
+
+    time = new Date(point.x)
+    time.setMinutes(time.getMinutes() - 1)
+    label = time.toISOString()
+    if (!times[label]) {
+      fullGraph.push({
+        x: label,
+        y: 0
+      })
+      times[label] = 0
+    }
+  }
+
+  fullGraph.sort((a, b) => {
+    if (a.x > b.x) {
+      return 1;
+    }
+    if (a.x < b.x) {
+      return -1;
+    }
+    return 0;
+  })
+
+  res.render('stats', {
+    local_url: localUrl,
+    vip: vip,
+    not_vip: not_vip,
+    entered: entered,
+    not_entered: not_entered,
+    graph: JSON.stringify(fullGraph)
+  })
+})
+
 app.post('/saveCreated', (req, res) => {
   const doc = {
     ...req.body,
